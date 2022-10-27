f729aec720d7c50f0429db0942ab8209e91606d2
308 <308@academicos.cesde.edu.co>
2022-10-27 08:49:19 -0500

    ok

--- config/web/views.py
+++ config/web/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+
+#IMPORTAR EL FORMULARIO A RENDER
+from web.formularios.formularioPlatos import FormularioPlatos
+
 # Create your views here.
 #las vistas en djangp son los CONTROLADORES
 
@@ -7,4 +11,13 @@ from django.shortcuts import render
 
 def Home(request):
     return render(request,'index.html')
+
+def Platos(request):
+
+    formulario=FormularioPlatos()
+    datosParaTemplate={
+        'formularioRegistro':formulario
+    }
+
+    return render(request,'platos.html',datosParaTemplate)
 
